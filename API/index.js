6d34d5745a1d655edcb9c638aa202e9ca26be95b
DANIEL MONTEIRO MICHAEL <daniel.mmichael@senacsp.edu.br>
2025-11-08 10:45:22 -0300

    atualizado em aula

--- API/index.js
+++ API/index.js
@@ -7,9 +7,26 @@ const app = express();
 
 // serviço = API
 app.get("/", (req, res) => {
-    res.send("Primeiro dia com NODE");
+    res.send("Respondendo: Primeiro dia com NODE");
 })
 
-app.listen(300, ()=>{
+//rota usuario/1
+app.get("/usuario/:id", (req, res) => {
+    let id = req.params.id;
+    res.send(`O cliente está querendo ver o usuário on o id ${id}`)
+
+})
+
+app.get("/aluno/:nome/:curso", (req, res) =>{
+    const novoAluno = {
+        nome: req.params.nome,
+        curso: req.params.curso
+    }
+    res.send(novoAluno);
+})
+
+
+//app listen precisa estar como a ultima opção
+app.listen(3000, ()=>{
     console.log("Iniciando minha API com EXPRESS");
 })
